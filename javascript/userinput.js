6cf6f25aeb62c84843357770361c4d66f1c1d2dd
diegomorales30 <diegovmo96@gmail.com>
2023-11-21 19:43:22 -0700

    changed up the logic on userinput script

--- javascript/userinput.js
+++ javascript/userinput.js
@@ -1,17 +1,9 @@
 var numberRange = [];
 var location = 0;
-var hGreenInput = document.getElementById("highGreenInput");
-var lGreenInput = document.getElementById("lowGreenInput");
-
-var hYellowInput = document.getElementById("highYellowInput");
-var lYellowInput = document.getElementById("lowYellowInput");
-
-var hRedInput = document.getElementById("highRedInput");
-var lRedInput = document.getElementById("lowRedInput");
 
 var submitColors = document.getElementById("submitColors");
 
-submitColors.addEventListener("click",submitColors());
+submitColors.addEventListener("click",submitColors(e));
 
 function checkHGreen(hGreenInput){
     if(hGreenInput <= 99.99){
@@ -32,17 +24,12 @@ function checkEveryColor(hColor,lColor,lstringcolor,hstringcolor,ltype,htype){
         console.log("The value of " + ltype + lstringcolor + " can not be greater than " + htype + hstringcolor);
         return true;
     }
-    var difference = hColor - lColor;
-    if(difference > 1 || difference > .01){
-        console.log("The difference between can not not be greater than 1 or .01");
-        return true;
-    }
     numberRange[location] = lColor;
     location = location + 1;
     return false;
 }
 
-function check(lRedInput){
+function checkLRed(lRedInput){
     if(lRedInput >= .01){
         console.log("The low red value has to be 0");
         return true
@@ -55,16 +42,25 @@ function check(lRedInput){
     location = location + 1;
     return false;
 }
-function submitColors(){
-   var isValidHGreen = checkHGreen(hGreenInput);
-   var isValidLGreen = checkEveryColor(hGreenInput,lGreenInput,"Green","Green","low","high");
-   var isValidHYellow = checkEveryColor(lGreenInput,hYellowInput,"Yellow","Green","low","high");
-   var isValidLYellow = checkEveryColor(hYellowInput,lYellowInput,"Yellow","Yellow","low","high");
-   var isValidHRed = checkEveryColor(lYellowInput,hRedInput,"Red","Yellow","low","high");
-   var isValidLRed = checkLRed(lRedInput);
+function submitColors(event){
+    var hGreenInput = parseFloat(document.getElementById("highGreenInput".value));
+    var lGreenInput = parseFloat(document.getElementById("lowGreenInput").value);
 
-   if((isValidHGreen || isValidLGreen) || (isValidHYellow || isValidLYellow) || (isValidHRed || isValidLRed)){
-    return false;
-   }
-   return true;
+    var hYellowInput = parseFloat(document.getElementById("highYellowInput").value);
+    var lYellowInput = parseFloat(document.getElementById("lowYellowInput").value);
+
+    var hRedInput = parseFloat(document.getElementById("highRedInput").value);
+    var lRedInput = parseFloat(document.getElementById("lowRedInput").value);
+
+    var isValidHGreen = checkHGreen(hGreenInput);
+    var isValidLGreen = checkEveryColor(hGreenInput,lGreenInput,"Green","Green","low","high");
+    var isValidHYellow = checkEveryColor(lGreenInput,hYellowInput,"Yellow","Green","low","high");
+    var isValidLYellow = checkEveryColor(hYellowInput,lYellowInput,"Yellow","Yellow","low","high");
+    var isValidHRed = checkEveryColor(lYellowInput,hRedInput,"Red","Yellow","low","high");
+    var isValidLRed = checkLRed(lRedInput);
+
+   if(!((isValidHGreen || isValidLGreen) || (isValidHYellow || isValidLYellow) || (isValidHRed || isValidLRed))){
+        event.preventDefault();
+    }
+    return true;
 }
